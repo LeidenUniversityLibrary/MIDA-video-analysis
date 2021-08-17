87483e44e2c1c294c781739f9e85d1d59c06398f
Ben Companjen <ben@companjen.name>
2021-08-17 09:21:02 +0200

    Save and load model as TF model

--- Transfer_learning.py
+++ Transfer_learning.py
@@ -5,39 +5,35 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
-from keras.models import model_from_json
-
+# from keras.models import model_from_json
+import sys
 # The `tf.keras.preprocessing.image_dataset_from_directory` can be used to generate a labeled dataset objects from a set of images on disk filed into class-specific folders.
 
-
+image_directory = sys.argv[1]
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    'Image', labels='inferred', label_mode='int',
+    image_directory, labels='inferred', label_mode='int',
     class_names=None, color_mode='rgb', image_size=(256,
     256), shuffle=True, seed= 42 , validation_split= 0.1, subset= 'training',
-    interpolation='bilinear', follow_links=False, smart_resize=False
+    interpolation='bilinear', follow_links=False #, smart_resize=False
 )
 
+class_names = train_ds.class_names
+print(class_names)
 
 
 
 validation_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    'Image', labels='inferred', label_mode='int',
+    image_directory, labels='inferred', label_mode='int',
     class_names=None, color_mode='rgb', image_size=(256,
     256), shuffle=True, seed= 42 , validation_split= 0.1, subset= 'validation',
-    interpolation='bilinear', follow_links=False, smart_resize=False
+    interpolation='bilinear', follow_links=False #, smart_resize=False
 )
 
+print("Number of training batches: %d" % tf.data.experimental.cardinality(train_ds).numpy())
+print("Number of validation batches: %d" % tf.data.experimental.cardinality(validation_ds).numpy())
 
-
-
-print("Number of training samples: %d" % tf.data.experimental.cardinality(train_ds))
-print("Number of validation samples: %d" % tf.data.experimental.cardinality(validation_ds))
-print("Number of test samples: %d" % tf.data.experimental.cardinality(test_ds))
-
-
-
-
+print("Resizing datasets")
 
 size = (150, 150)
 
@@ -45,10 +41,6 @@ train_ds = train_ds.map(lambda x, y: (tf.image.resize(x, size), y))
 validation_ds = validation_ds.map(lambda x, y: (tf.image.resize(x, size), y))
 
 # Random data augmentation
-
-
-
-
 data_augmentation = keras.Sequential(
     [
         layers.experimental.preprocessing.RandomFlip("horizontal"),
@@ -57,14 +49,8 @@ data_augmentation = keras.Sequential(
 )
 
 
-
-
-import numpy as np
-
-
 # Build a model
-
-
+print("Loading base model")
 
 base_model = keras.applications.VGG16(
     weights="imagenet",  # Load weights pre-trained on ImageNet.
@@ -77,7 +63,8 @@ base_model.trainable = False
 
 # Create new model on top
 inputs = keras.Input(shape=(150, 150, 3))
-x = data_augmentation(inputs)  # Apply random data augmentation
+# x = data_augmentation(inputs)  # Apply random data augmentation
+x = inputs  # Do not apply random data augmentation
 
 # Pre-trained Xception weights requires that input be normalized
 # from (0, 255) to a range (-1., +1.), the normalization layer
@@ -103,7 +90,7 @@ model.summary()
 
 # Train the top layer
 
-
+print("Starting compilation")
 
 model.compile(
     optimizer=keras.optimizers.Adam(),
@@ -111,17 +98,12 @@ model.compile(
     metrics=[keras.metrics.BinaryAccuracy()],
 )
 
+print("Starting fit")
 epochs = 20
 model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
 
 
-
 ## Save the model
-
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
+print("Saving model")
+model.save("./trained_model/")
 print("Saved model to disk")
--- apply_model.py
+++ apply_model.py
@@ -1,23 +1,13 @@
+# Load and apply the trained model
+import tensorflow as tf
+import cv2
 
 
-from keras.models import model_from_json
-
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
+model_path = sys.argv[1]
+loaded_model = tf.keras.models.load_model(model_path)
 print("Loaded model from disk")
-
-loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-
 print( type(loaded_model) )
 
-import cv2
-import os
 
 vidcap = cv2.VideoCapture('Payitaht Abdülhamid 1. Bölüm-MyNz3YvSEhk.mp4')
 
