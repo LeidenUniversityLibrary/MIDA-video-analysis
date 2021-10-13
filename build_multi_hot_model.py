2a985586b7db97b552fb5690393571fbbe10b25d
Ben Companjen <ben@companjen.name>
2021-10-13 14:07:00 +0200

    Change training for two symbols on Apple Metal
    - change Normalisation layer to Rescale
    - fill out columns in generators
    - remove shear and zoom
    - add metrics
    - add TensorBoard callbacks
    - add Conda dependencies

--- build_multi_hot_model.py
+++ build_multi_hot_model.py
@@ -7,6 +7,7 @@ import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
 import sys
+import datetime
 
 
 image_directory = sys.argv[1]
@@ -21,17 +22,15 @@ metadata = pd.read_csv(csv_filename)
 train_datagen = keras.preprocessing.image.ImageDataGenerator(
     # rescale=1./255,
     rotation_range=36,
-    shear_range=0.2,
-    zoom_range=0.2,
     horizontal_flip=True,
-    validation_split=0.2)
+    validation_split=0.3)
 
 train_generator = train_datagen.flow_from_dataframe(
     dataframe=metadata,
     directory=image_directory,
-    x_col='',
-    y_col=[''],
-    weight_col='',
+    x_col='filename',
+    y_col=['pentagram_visible', 'star_visible'],
+    weight_col=None,
     target_size=(150, 150),
     color_mode='rgb',
     class_mode='raw',
@@ -43,14 +42,14 @@ train_generator = train_datagen.flow_from_dataframe(
 
 validation_datagen = keras.preprocessing.image.ImageDataGenerator(
     # rescale=1./255,
-    validation_split=0.2)
+    validation_split=0.3)
 
 validation_generator = validation_datagen.flow_from_dataframe(
     dataframe=metadata,
     directory=image_directory,
-    x_col='',
-    y_col=[''],
-    weight_col='',
+    x_col='filename',
+    y_col=['pentagram_visible', 'star_visible'],
+    weight_col=None,
     target_size=(150, 150),
     color_mode='rgb',
     class_mode='raw',
@@ -61,14 +60,14 @@ validation_generator = validation_datagen.flow_from_dataframe(
 )
 
 
-print("Number of training samples: %d" % tf.data.experimental.cardinality(train_ds))
-print("Number of validation samples: %d" % tf.data.experimental.cardinality(validation_ds))
+# print("Number of training samples: %d" % tf.data.experimental.cardinality(train_ds))
+# print("Number of validation samples: %d" % tf.data.experimental.cardinality(validation_ds))
 
 
 
 
 # Build a model
-
+print("Start building the model")
 base_model = keras.applications.Xception(
     weights="imagenet",  # Load weights pre-trained on ImageNet.
     input_shape=(150, 150, 3),
@@ -81,15 +80,12 @@ base_model.trainable = False
 # Create new model on top
 inputs = keras.Input(shape=(150, 150, 3))
 
-# Pre-trained Xception weights requires that input be normalized
-# from (0, 255) to a range (-1., +1.), the normalization layer
-# does the following, outputs = (inputs - mean) / sqrt(var)
-norm_layer = keras.layers.experimental.preprocessing.Normalization()
-mean = np.array([127.5] * 3)
-var = mean ** 2
-# Scale inputs to [-1, +1]
-x = norm_layer(inputs)
-norm_layer.set_weights([mean, var])
+# Pre-trained Xception weights requires that input be scaled
+# from (0, 255) to a range of (-1., +1.), the rescaling layer
+# outputs: `(inputs * scale) + offset`
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x = scale_layer(inputs)
+
 
 # The base model contains batchnorm layers. We want to keep them in inference mode
 # when we unfreeze the base model for fine-tuning, so we make sure that the
@@ -97,28 +93,35 @@ norm_layer.set_weights([mean, var])
 x = base_model(x, training=False)
 x = keras.layers.GlobalAveragePooling2D()(x)
 x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
-outputs = keras.layers.Dense(3, activation='sigmoid')(x)
+outputs = keras.layers.Dense(2, activation='sigmoid')(x)
 model = keras.Model(inputs, outputs)
 
 model.summary()
 
 
 # Train the top layer
-
+print("Start compiling the model")
 model.compile(
-    optimizer=keras.optimizers.Adam(),
+    optimizer=keras.optimizers.Nadam(),
     loss=keras.losses.BinaryCrossentropy(from_logits=False),
-    metrics=[[keras.metrics.BinaryAccuracy()],
-        [keras.metrics.BinaryAccuracy()],
-        [keras.metrics.BinaryAccuracy()]]
+    metrics=[keras.metrics.BinaryAccuracy(),
+        keras.metrics.FalseNegatives(),
+        keras.metrics.FalsePositives(),
+        keras.metrics.TrueNegatives(),
+        keras.metrics.TruePositives()
+    ]
 )
 
-epochs = 20
-model.fit(train_generator, epochs=epochs, validation_data=validation_generator)
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+print("Start fitting the model")
+epochs = 25
+model.fit(train_generator, epochs=epochs, validation_data=validation_generator, 
+          callbacks=[tensorboard_callback])
 
 
 ## Save the model
-
+print("Start saving the model to disk")
 model.save(model_directory)
 
 print("Saved model to disk")
